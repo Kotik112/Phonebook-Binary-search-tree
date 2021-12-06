9d8f1a5ed75a15f95422c7da3197afaef1e36d26
Arman I <Armaniqbal@gmail.com>
2021-12-06 13:05:05 +0100

    comments added

--- io_phonebook.py
+++ io_phonebook.py
@@ -1,23 +1,27 @@
 def phonebook_input():
     """Takes input for the phone book entry."""
+    #input for first name
     first_name = input("Enter the first name: ")
     if first_name.isdigit():
         print("The first name cannot be numeric.")
         return None
     first_name = first_name.lower().capitalize()
 
+    #Input for second name
     second_name = input("Enter the surname: ")
     if second_name.isdigit():
         print("The second name cannot be numeric.")
         return None
     second_name = second_name.lower().capitalize()
 
+    #Input for phone number
     phone_nr = input("Enter the phone number: ")
     if phone_nr.islower() or phone_nr.isalpha() or phone_nr.isalnum():
         print("THe telephone number must be numeric.")
         return None
     phone_nr = int(phone_nr)
 
+    #Return all input
     return first_name, second_name, phone_nr
 
 def main_menu():
